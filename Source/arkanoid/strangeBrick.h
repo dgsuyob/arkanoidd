0ded6f17aafa0e9e15e8bf366ab43dc2893c3a1b
Dgsuyob <davidsuyob@gmail.com>
2022-12-13 16:20:35 -0400

    2do parcial

---
+++ Source/arkanoid/strangeBrick.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Brick.h"
+#include "strangeBrick.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_API AstrangeBrick : public ABrick
+{
+	GENERATED_BODY()
+	
+};
