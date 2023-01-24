9eedcb3517546c6c45a445fe1228d20381de219f
Dgsuyob <davidsuyob@gmail.com>
2023-01-24 08:31:40 -0400

    enviar

---
+++ Source/arkanoid/bonusCapsule.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "capsuleFactory.h"
+#include "bonusCapsule.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_API AbonusCapsule : public AcapsuleFactory
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/arkanoid/capsuleFactory.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "capsuleFactory.h"
+
+// Sets default values
+AcapsuleFactory::AcapsuleFactory()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AcapsuleFactory::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AcapsuleFactory::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
